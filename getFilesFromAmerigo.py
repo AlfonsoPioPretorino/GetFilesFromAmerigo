e9ee70df54b30d6aa5c51a99a064e900fc6c6227
Alfonso Pio Pretorino <a.pretorino@studenti.uniba.it>
2022-05-07 10:59:58 +0200

    Added download progress bar

--- getFilesFromAmerigo.py
+++ getFilesFromAmerigo.py
@@ -5,14 +5,22 @@ import sys
 import io
 from bs4 import BeautifulSoup
 from sys import stdout
+from tqdm import tqdm
 
-def _restart_line():
-    stdout.write('\r')
-    stdout.flush()
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+def download_url(url, output_path):
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
 
 def startDownload(url):
     index = 1
-    reqs = requests.get(url)
+    reqs = requests.get(url, stream=True)
     soup = BeautifulSoup(reqs.text, 'html.parser')
     
     urls = []
@@ -23,15 +31,24 @@ def startDownload(url):
             print(index, ")", link.get('href'))
             currentLink = link.get('href')
             if " " in link.get('href'):
-                continue0
+                continue
             urlDownload = url+currentLink
-            urllib.request.urlretrieve(urlDownload, 'C:/Users/Alfonso/Desktop/test'+currentLink)
+            path = 'C:/Users/Alfonso/Desktop/test'+currentLink
+            download_url(urlDownload, path)
+            
+                        
             index += 1
 
+
+print("╔═════════════════════════════════════════════════════════════════════════════════╗")
+print("║░░░░░░░░░░░░░░░░░░░░░░░░░░░GET FILES FORM AMERIGO░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║")
+print("║                                By Fonzi-Lab                                     ║")
+print("║                     GitHub: https://github.com/Fonzi-Lab                        ║")
+print("╚═════════════════════════════════════════════════════════════════════════════════╝")
+
 url = input("Inserire URL Target. Inserire 0 per usare quello di default.\n")
-choice = int(url)
-print(choice == 0)
-if choice == 0:
+
+if url == 0:
     url = 'http://192.168.1.15:8080'
 else:
     url = 'http://'+ url
